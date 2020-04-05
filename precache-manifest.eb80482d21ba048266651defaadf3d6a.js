1b8ad8eb0bf36fc3af65547d6d6e9892c24cba6f
Suraj Gawas <suraj.gawas@nike.com>
2020-04-05 01:06:20 -0700

    Updates

---
+++ precache-manifest.eb80482d21ba048266651defaadf3d6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0312bf264bd7f97cbff0791d46080e",
+    "url": "/myrobofriends/index.html"
+  },
+  {
+    "revision": "ee4c6cdc7d79a846b984",
+    "url": "/myrobofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d33e429cf2f2b33d7911",
+    "url": "/myrobofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee4c6cdc7d79a846b984",
+    "url": "/myrobofriends/static/js/2.2c1c7242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myrobofriends/static/js/2.2c1c7242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33e429cf2f2b33d7911",
+    "url": "/myrobofriends/static/js/main.9294700d.chunk.js"
+  },
+  {
+    "revision": "a88d3e2520695f8cadc1",
+    "url": "/myrobofriends/static/js/runtime-main.195d77d0.js"
+  }
+]);
